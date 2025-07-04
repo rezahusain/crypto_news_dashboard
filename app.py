eb12af05d5f83fb92fe041f894026a81af6b09ac
rezar <rhusai2@uic.edu>
2025-07-04 12:55:57 -0500

    Added more design to the landing and news page

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template
-import json
-import sqlite3
+from models import SessionLocal, NewsArticle
+from datetime import datetime
 
 app = Flask(__name__)
 DATABASE_PATH ='instance/news.db'
 
 @app.route('/')
 def index():
-    connect = sqlite3.connect(DATABASE_PATH)
-    cursor = connect.cursor()
-    cursor.execute('SELECT * FROM news_articles;')
+    # You can pass preview articles to the landing page too
+    session = SessionLocal()
+    sample_articles = session.query(NewsArticle).order_by(NewsArticle.published_at.desc()).limit(6).all()
+    session.close()
+    return render_template('landing_page.html', sample_articles=sample_articles)
 
-    news = cursor.fetchall()
-    return render_template('index.html', articles=news)
+@app.route('/news')
+def news():
+    session = SessionLocal()
+    articles = session.query(NewsArticle).order_by(NewsArticle.published_at.desc()).all()
+    session.close()
+    return render_template('news.html', articles=articles)
 
 if __name__ == '__main__':
     app.run(debug=True)
